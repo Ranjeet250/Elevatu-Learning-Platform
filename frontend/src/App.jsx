74adc8f94ea18b4ef028bee5fa2e52630b5b3664
Ranjeet250 <148352795+Ranjeet250@users.noreply.github.com>
2025-07-10 00:32:39 +0530

    Fix: Correct Footer path case issue for Vercel

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Roadmap from "./pages/Roadmap";
 import Navbar from "./components/Navbar";
-import Footer from "./components/footer";
+import Footer from "./components/Footer";
 import Login from "./pages/Login";
 import Contact from "./pages/Contact";
 import Register from "./pages/Register";
